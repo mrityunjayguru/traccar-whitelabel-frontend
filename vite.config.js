ee6da47cdf69c92fe2e1494b249a6531d9f73604
darshit-codes <darshit.code@email.com>
2026-05-02 13:59:21 +0530

    FIX : Changes

---
+++ vite.config.js
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
+
+/* eslint-disable no-template-curly-in-string */
+export default defineConfig(() => ({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api/socket': 'wss://stagetrackwhitelabel.trackroutepro.com/',
+      '/api': 'https://stagetrackwhitelabel.trackroutepro.com/',
+      '/auth-api': {
+          target: 'https://app.trackroutepro.com',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/auth-api/, ''),
+        },
+    },
+    host: true,
+    allowedHosts: ['trackfront.trackroutepro.com'],
+    
+  },
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    svgr(),
+    tailwindcss(),
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png'],
+      workbox: {
+        navigateFallbackDenylist: [/^\/api/],
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+        globPatterns: ['**/*.{js,css,html,woff,woff2,mp3}'],
+      },
+      manifest: {
+        short_name: '${title}',
+        name: '${description}',
+        theme_color: '${colorPrimary}',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+}));
