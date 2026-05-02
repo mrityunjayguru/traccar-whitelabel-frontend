25dcbba0bfa4f48cc0de8d3d65835aa09abf3494
darshit-codes <darshit.code@email.com>
2026-05-02 13:55:36 +0530

    FIX : chnges

--- src/login/LoginLayout.jsx
+++ src/login/LoginLayout.jsx
@@ -6,7 +6,7 @@ const LoginLayout = ({ children }) => {
     <main className="flex justify-center items-center min-h-screen w-full bg-[#DDE1E8]  p-4 md:p-10">
       <div className="flex flex-col md:flex-row items-stretch w-full max-w-[1300px] gap-6 md:gap-10">
         <div className="relative flex flex-col justify-between items-start w-full md:w-[60%] min-h-[300px] md:min-h-[450px] p-8 md:p-12 bg-[#1A1C1E] rounded-[30px] overflow-hidden shadow-xl">
-          <div
+          <div 
             className="absolute inset-0 bg-cover bg-center z-0"
             style={{ backgroundImage: "url('/src/resources/images/base.svg')" }}
           />
@@ -28,6 +28,7 @@ const LoginLayout = ({ children }) => {
             </div>
           </div>
         </div>
+        
         <Paper
           square={false}
           elevation={0}
